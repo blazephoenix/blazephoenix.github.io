45394a222b998d6c1ec6276ffc16390a3875ae05
Blaze <16274366+Blazephoenix@users.noreply.github.com>
2020-01-04 22:29:55 +0530

    changed theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
 import React from "react"
-import Header from "../components/header"
-import Container from "../components/container"
-import { graphql } from "gatsby"
+import { Link } from "gatsby"
 
-export default ({ data }) => (
-<Container>
-    <Header/>
-    <h1>{data.site.siteMetadata.title}</h1>
-    <p>This is a started template for gatsby.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-</Container>
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
 )
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+
+export default IndexPage
