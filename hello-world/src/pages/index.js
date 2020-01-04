290035aca6b598bd10a25ea21c2eb89bf86bad01
Blaze <16274366+Blazephoenix@users.noreply.github.com>
2020-01-04 18:51:56 +0530

    Graphql filesys

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,12 +1,22 @@
 import React from "react"
 import Header from "../components/header"
 import Container from "../components/container"
+import { graphql } from "gatsby"
 
-export default () => (
+export default ({ data }) => (
 <Container>
     <Header/>
-    <h1>Hello Gatsby!</h1>
+    <h1>{data.site.siteMetadata.title}</h1>
     <p>This is a started template for gatsby.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
 </Container>
 )
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
