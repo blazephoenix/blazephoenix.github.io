363df9cd9a7cf3eba21da6d083092ec7b91571d7
Blaze <16274366+Blazephoenix@users.noreply.github.com>
2020-01-06 21:40:47 +0530

    changed theme color & fixed horizontal scroll issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Lazy Dev`,
+    title: `Blaze codes`,
     author: `Tanmay Naik (Blaze)`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    description: `My own sweet spot on the internet`,
+    siteUrl: `https://blazephoenix.github.io/blog`,
     social: {
       twitter: `blazephoenix2`,
       dev: `blazephoenix`
@@ -63,11 +63,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Blaze codes`,
+        short_name: `Blazecodes`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#e53939`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
       },
